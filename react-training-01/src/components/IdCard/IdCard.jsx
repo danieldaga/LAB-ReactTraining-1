d0057886825b1543690ee0322cde9db7ab499dc1
danieldaga <daniel@arhrestauracion.com>
2023-01-19 20:39:54 +0100

    ex1 done not birth

--- react-training-01/src/components/IdCard/IdCard.jsx
+++ react-training-01/src/components/IdCard/IdCard.jsx
@@ -5,11 +5,11 @@ const IdCard =({firstName, lastName, gender, height, birth,picture})=>{
         <div className="img-person"><img src={picture} alt="" width='100px'/></div>
         <div className="data">
             <p>
-                <span>First name: </span>{firstName} <br />
-                <span>Last name: </span>{lastName}<br />
-                <span>gender: </span>{gender}<br />
-                <span>height: </span>{height}<br />
-                <span>birth: </span>{birth}
+                <b>First name: </b>{firstName} <br />
+                <b>Last name: </b>{lastName}<br />
+                <b>gender: </b>{gender}<br />
+                <b>height: </b>{height}<br />
+                <b>birth: </b>{birth}
             </p>
         </div>
     </div>
