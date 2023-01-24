27eec7761fcb8e64dd2d71c7c8a8e5939c8b2451
danieldaga <daniel@arhrestauracion.com>
2023-01-24 22:27:24 +0100

    drivercard done

--- react-training-01/src/components/DriverCard/DriverCard.jsx
+++ react-training-01/src/components/DriverCard/DriverCard.jsx
@@ -1,12 +1,15 @@
+import './DriverCard.css'
+import Rating from '../Rating/Rating';
+
 const DriverCard = ({img, name, rating, car}) => {
 
 
     return(
-        <div>
+        <div className='driver-contain'>
             <div className="image-drive"><img src={img} alt={name} className='driver-pilot-img'/></div>
             <div className="driver-pilot">
                 <h3 className="name">{name}</h3>
-                <p className="rating"></p>
+                <p className="rating"><Rating>{rating}</Rating></p>
                 <p className="car-data">{car.model}-{car.licensePlate}</p>
             </div>
         </div>
