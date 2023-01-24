128375722fa18e41e84ce40ac0b53a9d7b05bea0
danieldaga <daniel@arhrestauracion.com>
2023-01-24 22:46:54 +0100

    drivercard done

--- react-training-01/src/components/LikeButton/LikeButton.jsx
+++ react-training-01/src/components/LikeButton/LikeButton.jsx
@@ -7,12 +7,26 @@ const LikeButton = () =>{
     const [color, setColor] = useState(0)
     
     const colorsButton = ['purple','blue','green','yellow','orange','red']
+    const [colorNum, setColorNum] = useState(colorsButton[color])
+    
+    function backgroundC() {
+
+        setCount(count + 1)
+
+        if (color === colorsButton.length - 1) {
+            setColorNum(colorsButton[0])
+            setColor(0)
+        }else{
+            setColorNum(colorsButton[color + 1])
+            setColor(color + 1)
+        }
+    }
+    
     return(
         <button 
         className='button-like' 
-        onChange={()=>setColor(color+1)} 
-        onClick={()=>setCount(count+1)}
-        style={{backgroundColor: colorsButton[count]}}
+        onClick={()=>backgroundC()}
+        style={{backgroundColor: colorNum}}
         >{count} Likes</button>
     )
 
